2a1999a05c451a301940c7c07fc5189274b84d20
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2025-01-26 01:47:28 +0200

    changed home to mine

--- src/data/seo.js
+++ src/data/seo.js
@@ -3,22 +3,16 @@ const SEO = [
 		page: "home",
 		description:
 			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["bassant", "bassant N", "bassant Nayanajith"],
 	},
 
 	{
 		page: "about",
 		description:
 			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["bassant", "bassant N", "bassant Nayanajith"],
 	},
 
-	{
-		page: "articles",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
-	},
 
 	{
 		page: "projects",
